43e4b74bd9bad24cc1c279299e82f8e7a8138298
wadezigh96 <xyzkames@gmail.com>
2026-09-19 17:31:41 +0700

    feat: initialize Privy Solana wallet provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { PrivyProvider } from "@privy-io/react-auth";
 import App from "./App";
 import "./index.css";
 
+const privyAppId =
+  import.meta.env.VITE_PRIVY_APP_ID || "cmu88yc4n02hl0cjqmqhcgjx2";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PrivyProvider
+      appId={privyAppId}
+      config={{
+        embeddedWallets: {
+          solana: { createOnLogin: "all-users" },
+        },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PrivyProvider>
   </React.StrictMode>
 );
